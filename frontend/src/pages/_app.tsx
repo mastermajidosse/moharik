bf7699ceef4552dea252ca6d8c3c5d3308c67c83
bilal essofyany <soufyani.bilal@gmail.com>
2022-06-08 23:59:05 +0100

    add nprogress

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -3,8 +3,14 @@ import type { AppProps } from "next/app";
 import Layout from "../components/layout/Layout";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { Router } from "next/router";
+import { done, start } from "nprogress";
+// import "nprogress/nprogress.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  Router.events.on("routeChangeStart", () => start());
+  Router.events.on("routeChangeComplete", () => done());
+  Router.events.on("routeChangeError", () => done());
   return (
     <Layout>
       <Component {...pageProps} />
