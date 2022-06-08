c5280c4ae456e3982f721c4abd824f6f289c7d42
bilal essofyany <soufyani.bilal@gmail.com>
2022-06-08 22:32:36 +0100

    fix motos

--- frontend/src/data/motos.ts
+++ frontend/src/data/motos.ts
@@ -1,6 +1,6 @@
 export const motos = [
-  "whenever there is a will",
+  "whenever there is a will there's a way",
   "if you can dream it you can do it",
   "focus on your goal",
-  "self belief and hard is the key",
+  "self belief and hard work is the key",
 ];
