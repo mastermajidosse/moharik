9be16aa8236dce8b82a8d376341bfc655a3519c7
bilal essofyany <soufyani.bilal@gmail.com>
2022-06-10 20:16:10 +0100

    add cors param to env file

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ import cors from "cors"
 
 const app = express()
 app.use(cors({
-   origin: 'http://localhost:3000',
+   origin: process.env.CLIENT_URL || "*",
   optionsSuccessStatus: 200
 }))
 app.use(express.json())
