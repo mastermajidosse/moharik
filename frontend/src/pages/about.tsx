84bc842a79a12034415cc532322b873b981e2adf
bilal essofyany <soufyani.bilal@gmail.com>
2022-06-12 13:27:35 +0100

    fix responsive issue in about page

--- frontend/src/pages/about.tsx
+++ frontend/src/pages/about.tsx
@@ -1,39 +1,38 @@
 import Head from "next/head";
 export default function AboutPage() {
   return (
-    <div className="mt-10 py-16 md:py-24">
+    <div className="container mt-10 py-16 md:py-24">
       <Head>
         <title>Moharik | About</title>
       </Head>
-      <section className="container">
+      <section className="">
         <h1 className="text-2xl md:text-3xl text-dark font-bold mb-6 text-center">
           About <span className="text-primary">Moharik</span>
         </h1>
-        <div className="flex flex-col gap-4 text-center mx-40 my-14">
-          <p className="text-lightDark font-medium text-sm md:text-base leading-relaxed tracking-wide">
-            We believe that innovations and new ideas can make this world
-            better,and we also believe that we can make your unique idea or
-            project POSSIBLE by showing your project to investors or supporters
-            to take your project to the next level. Welcome to MOHARIK, the
-            right place to make your dreams real.
-          </p>
-        </div>
+        <p className="w-full md:w-2/3 mx-auto text-lightDark font-medium text-sm md:text-base leading-relaxed tracking-wide text-center">
+          We believe that innovations and new ideas can make this world
+          better,and we also believe that we can make your unique idea or
+          project <span className="decoration-primary underline">POSSIBLE</span>{" "}
+          by showing your project to investors or supporters to take your
+          project to the next level. Welcome to{" "}
+          <span className="text-primary">MOHARIK</span>, the right place to make
+          your dreams real.
+        </p>
       </section>
       {/* pic section */}
-      <section className="container my-16">
+      <section className="my-8 md:my-16 w-full md:w-2/3 mx-auto">
         <figure className="relative rounded-md overflow-hidden">
           <img src="/assets/images/about.jpg" alt="" />
         </figure>
       </section>
-      <div className="flex flex-col gap-4 text-center mx-36 my-14">
-        <p className="text-lightDark font-medium text-sm md:text-base leading-relaxed tracking-wide">
-          Many small business owners in this world couldn&#39;t make their
-          dreams come true,because they didn&#39;t have the financial
-          opportunity to commercialize and grow their projects, MOHARIK gives
-          you the opportunity to meet the investors and like minded creative
-          people to get what you need to make your dream come true.
-        </p>
-      </div>
+      <p className="w-full md:w-2/3 mx-auto text-lightDark font-medium text-sm md:text-base leading-relaxed tracking-wide text-center">
+        Many small business owners in this world couldn&#39;t make their dreams
+        come true,because they didn&#39;t have the financial opportunity to
+        commercialize and grow their projects,{" "}
+        <span className="text-primary">MOHARIK</span> gives you the opportunity
+        to meet the investors and like minded creative people to get what you
+        need to make your dream come true.
+      </p>
     </div>
   );
 }
