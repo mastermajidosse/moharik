6984a8a68017368a0f6c815bf7566d128429a7e1
otmane el majid <mastermajidosse@gmail.com>
2022-10-07 10:42:43 +0100

    prod7

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,21 +3,15 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { i18n } = require("./next-i18next.config");
 
-// const nextConfig = {
-//   reactStrictMode: true,
-//   i18n,
-//   eslint: {
-//     // Warning: This allows production builds to successfully complete even if
-//     // your project has ESLint errors.
-//     ignoreDuringBuilds: true,
-//   },
-// };
+const nextConfig = {
 
-// module.exports = nextConfig;
-
-
-module.exports = {
+  // reactStrictMode: true,
   i18n,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   async headers() {
     return [
       {
@@ -31,5 +25,7 @@ module.exports = {
         ]
       }
     ]
-  }
+  },
 };
+
+module.exports = nextConfig;
