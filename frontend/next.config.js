5cd6f799a861ded9f4634b14373cf279a7a5f809
otmane el majid <mastermajidosse@gmail.com>
2022-10-07 09:52:26 +0100

    v2

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -7,15 +7,9 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        // matching all API routes
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      }
+        source: '/api/:path*',
+        destination: 'http://api.moharik.ma/:path*',
+      },
     ]
   },
   reactStrictMode: true,
