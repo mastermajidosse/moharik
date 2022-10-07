7ed92ab1cb58ee72671221154113f42b55f8c6fe
otmane el majid <mastermajidosse@gmail.com>
2022-10-07 11:43:41 +0100

    prod9

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -16,7 +16,7 @@ const nextConfig = {
     return [
       {
         // matching all API routes dd
-        source: "/api/(.*)",
+        source: "/api/users/(.*)",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
